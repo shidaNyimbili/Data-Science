a68394101c0f7dfc69a34d7b300b74a2689bf046
Shida Nyimbili <nyimbilishida@gmail.com>
2025-05-17 20:16:03 +0200

    ns

--- Scripts/ML/Part 2 - Regression/Section 6 - Polynomial Regression/R/polynomial_regression.R
+++ Scripts/ML/Part 2 - Regression/Section 6 - Polynomial Regression/R/polynomial_regression.R
@@ -1,11 +1,41 @@
 # Polynomial Regression
 
-# Importing the dataset
-dataset = read.csv('Position_Salaries.csv')
-dataset = dataset[2:3]
+library(tidyverse)
 
+# Importing the dataset
+dataset <- read.csv('Position_Salaries.csv')
+
+#review the data
+colSums(is.na(dataset))   # Check total missing values per column
+
+str(dataset) #Check the structure of the dataset
+
+head(dataset) #Check the first few rows of the dataset
+
+setNames(seq_along(colnames(dataset)), colnames(dataset)) # View column names with their index positions
+
+dataset5 <- dataset[2:3]
+
+dataset2 <- select(dataset, c(Level, Salary))
+
+dataset3 <- dataset %>%
+  select(Level, Salary) %>%
+  filter(Salary > 55000)
+
+dataset <- dataset %>%
+  select(Level, Salary)
+
+dataset4
+dataset3
+dataset2
+dataset5
+dataset
+
+
+# set RNG seed for reproducibility
 # Splitting the dataset into the Training set and Test set
-# # install.packages('caTools')
+
+
 # library(caTools)
 # set.seed(123)
 # split = sample.split(dataset$Salary, SplitRatio = 2/3)
@@ -17,8 +47,21 @@ dataset = dataset[2:3]
 # test_set = scale(test_set)
 
 # Fitting Linear Regression to the dataset
-lin_reg = lm(formula = Salary ~ .,
+lin_reg <- lm(formula = Salary ~ .,
              data = dataset)
+
+summary(lin_reg)
+
+# Predicting a new result with Linear Regression
+
+pred <- predict(lin_reg, newdata = dataset)
+
+pred
+
+# Compare predicted values with actual values linear
+results <- data.frame(Actual = dataset$Salary, Predicted = pred)
+
+results
 
 # Fitting Polynomial Regression to the dataset
 dataset$Level2 = dataset$Level^2
@@ -26,6 +69,42 @@ dataset$Level3 = dataset$Level^3
 dataset$Level4 = dataset$Level^4
 poly_reg = lm(formula = Salary ~ .,
               data = dataset)
+summary(poly_reg)
+
+# Predicting a new result with Polynomial Regression
+
+predpo <- predict(poly_reg, newdata = dataset)
+
+predpo
+
+# Compare predicted values with actual values polynomial
+results <- data.frame(Actual = dataset$Salary, Predicted = predpo)
+
+results
+
+# Reshape data to long format for ggplot
+results_long <- results %>%
+  mutate(Index = row_number()) %>%
+  pivot_longer(cols = c("Actual", "Predicted"),
+               names_to = "Type",
+               values_to = "Salary")
+
+
+results_long
+
+write.csv(results_long, "results_long.csv", row.names = FALSE)
+
+view(results_long)
+
+# Plot actual vs predicted with different colors polynomial regression
+ggplot(results_long, aes(x = Index, y = Salary, color = Type)) +
+  geom_point(size = 3) +
+  labs(title = "Actual vs Predicted Profits",
+       x = "Observation",
+       y = "Profit") +
+  theme_minimal() +
+  scale_color_manual(values = c("Actual" = "blue", "Predicted" = "orange"))
+
 
 # Visualising the Linear Regression results
 # install.packages('ggplot2')
@@ -33,7 +112,7 @@ library(ggplot2)
 ggplot() +
   geom_point(aes(x = dataset$Level, y = dataset$Salary),
              colour = 'red') +
-  geom_line(aes(x = dataset$Level, y = predict(lin_reg, newdata = dataset)),
+  geom_line(aes(x = dataset$Level, y = pred),
             colour = 'blue') +
   ggtitle('Truth or Bluff (Linear Regression)') +
   xlab('Level') +
